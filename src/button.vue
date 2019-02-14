aa1e46b7008e4b9026c8bb5f5fd61dba3dc3663a
dflk <904885508@qq.com>
2019-02-14 22:13:08 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,21 +1,57 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="iconPosition">
+        <svg class="icon" v-if="icon" >
+            <use :xlink:href="`#icon-${icon}`"></use>
+        </svg>
+        <span class="content">
+            <slot name="text"></slot>
+        </span>
+    </button>
 </template>
 
 <script>
- export default {}
+    export default {
+        // props: ['icon','iconPosition']
+        props:{
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function(value){
+                    return !(value !== 'left' && value !== 'right');
+                }
+            },
+
+        }
+    }
 </script>
 
 <style lang="scss">
-    .g-button{
+    .g-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{ border-color: var(--border-cover-hover); }
-        &:active{ background: var(--button-active-bg) }
-        &:focus{ outline: none }
+        &.right{
+            .content{order: 1}
+            .icon{order: 2}
+        }
+        &:hover {
+            border-color: var(--border-cover-hover);
+        }
+
+        &:active {
+            background: var(--button-active-bg)
+        }
+
+        &:focus {
+            outline: none
+        }
     }
 </style>
