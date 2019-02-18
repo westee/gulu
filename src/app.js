a72c3b6d13270ddeb430e05c974dab824c63bf68
dflk <904885508@qq.com>
2019-02-19 01:30:32 +0800

    添加input.vue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 import Chai from 'chai'
 import ChaiSpies from 'chai-spies'
 Chai.use(ChaiSpies)
@@ -9,6 +11,7 @@ Chai.use(ChaiSpies)
 Vue.component('g-icon', Icon)
 Vue.component('g-button', Button)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 
 new Vue({
     el: '#app',
@@ -20,77 +23,3 @@ new Vue({
 })
 
 
-//单元测试
-{
-    var expect = Chai.expect
-    const constructor = Vue.extend(Button) //生成构造函数
-    const btn = new constructor({ // 创建实例并传参
-        propsData:{
-            icon: 'zan'
-        }
-    })
-    btn.$mount() //将实例挂载到#test节点
-    let use = btn.$el.querySelector('use')
-    let iconName = use.getAttribute('xlink:href')
-    expect(iconName).to.equal('#icon-zan');
-    btn.$el.remove()
-    btn.$destroy()
-    /**
-     * actual: "#icon-zan"  //实际上是
-     * expected: "#icon-za"  //期望的是
-     * message: "expected '#icon-zan' to equal '#icon-za'" //提示信息
-     */
-}
-
-{
-    const constructor = Vue.extend(Button) //生成构造函数
-    const btn = new constructor({ // 创建实例并传参
-        propsData:{
-            icon: 'zan',
-            loading: true
-        }
-    })
-    btn.$mount('#test') //将实例挂载到#test节点
-    let use = btn.$el.querySelector('use')
-    let iconName = use.getAttribute('xlink:href')
-    expect(iconName).to.equal('#icon-loading');
-    btn.$el.remove()
-    btn.$destroy()
-}
-
-{
-    let div = document.createElement('div')
-    // div.id = 'assert'
-    // div.style.visibility = 'hidden'
-    document.body.appendChild(div)
-    const constructor = Vue.extend(Button) //生成构造函数
-    const btn = new constructor({ // 创建实例并传参
-        propsData:{
-            loading: true,
-            iconPosition:'right'
-        }
-    })
-    btn.$mount(div)
-    let svg = btn.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.equal('2');
-    btn.$el.remove()
-    btn.$destroy()
-}
-
-//test click
-{
-    const constructor = Vue.extend(Button)
-    const btn = new constructor({
-        propsData:{
-            icon: 'zan',
-            loading: true
-        }
-    }).$mount()
-    let spy = Chai.spy(function(){})
-    btn.$on('click', spy)
-    btn.$el.click()
-    expect(spy).to.have.been.called();
-    btn.$el.remove()
-    btn.$destroy()
-}
---
+++ src/input.vue
@@ -0,0 +1,90 @@
+<template>
+    <div :class="{'wrapper': true,'error': error} ">
+        <input :value="inputPlaceholder" :disabled="inputDisabled" :readonly="readonly" type="text">
+        <template>
+            <Icon name="warn" v-if="error"></Icon>
+            <span class="message">{{ error }}</span>
+        </template>
+
+    </div>
+
+</template>
+
+<script>
+    import  Icon from './icon'
+    export default {
+        name: 'g-input',
+        components:{
+          'Icon': Icon
+        },
+        props: {
+            inputPlaceholder: {
+                type: String,
+            },
+            inputDisabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly:{
+                type: Boolean,
+                default: false
+            },
+            error:{
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $red: #f14530;
+    .wrapper {
+        display: inline-flex;align-items: center;
+        > input {
+            padding: 9px 8px;
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+
+            &:hover {
+                border-color: $border-color-hover;
+            }
+
+            &:focus {
+                box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.5);
+                outline: none;
+            }
+            /*&:disabled {*/
+                /*outline: #333;*/
+                /*border-color: #aaa;*/
+                /*color: #ccc;*/
+            /*}*/
+            &[disabled]{
+                border-color: #aaa;
+                color: #ccc;
+                cursor: not-allowed;
+            }
+
+        }
+        &.error{
+            :not(:last-child){
+                margin-right: 0.5em;
+            }
+            > input{
+                border-color: $red;
+                outline: $red;
+                box-shadow: none;
+            }
+            .message{
+                color: $red;
+            }
+        }
+        .icon{
+            color:$red;
+        }
+    }
+</style>
