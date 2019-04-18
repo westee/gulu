145e690d49c7c4a01bb598d15ee840c5cad55413
dflk <904885508@qq.com>
2019-04-18 23:25:42 +0800

    使用ESlint和prettier

---
+++ src/demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <g-button>
+      hi
+    </g-button>
+  </div>
+</template>
+<script>
+import Button from './button'
+export default {
+  components:{
+    'g-button': Button
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
